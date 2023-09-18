0ef466bdf2e57a2d791592e9e5600f0baf80585d
lyndossantos <108936981+lyndossantos@users.noreply.github.com>
2023-09-18 11:40:56 -0300

    Add files via upload

--- script/principal.js
+++ script/principal.js
@@ -12,4 +12,12 @@ eq2grao = (a,b,c) =>{
     return "x1" + div(soma(-b,raiz(delta)),mult(2,a)) +
            "x2" + div(sub(-b,raiz(delta)),mult(2,a));
 }
-escrever(eq2grao(1,3,-8));
+let a = "";
+let b = "";
+let op = "";
+let resultado = 0;
+
+function digitando (tecla){
+    a =+ tecla;
+    alert (a);
+}
